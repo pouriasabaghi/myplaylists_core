b693acbcbe594836ab49c58da2547e7068821c97
Pouria Sabaghi <pouria.782@gmail.com>
2024-11-13 17:37:37 +0330

    test: test

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->statefulApi();
+        $middleware->validateCsrfTokens([
+            'telegram-webhook',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
