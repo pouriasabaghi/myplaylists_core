b790b3bedca16b859215a6edc425b03ad070e856
pouriasabaghi <pouriasabaghi@gmail.com>
2025-02-13 17:25:39 +0330

    feat: inline query and send song from bot to server

--- app/Http/Controllers/TelegramBotController.php
+++ app/Http/Controllers/TelegramBotController.php
@@ -2,16 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use App\Interfaces\AiInterface;
-use App\Models\Playlist;
 use App\Models\Song;
 use App\Models\User;
 use App\Services\SongService;
 use App\Services\TelegramBotService;
-use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Http\Request;
+use Telegram\Bot\FileUpload\InputFile;
 use Telegram\Bot\Laravel\Facades\Telegram;
-use Telegram\Bot\Keyboard\Keyboard;
+use App\Interfaces\AiInterface;
 
 class TelegramBotController extends Controller
 {
@@ -30,17 +27,18 @@ class TelegramBotController extends Controller
 
         $this->message = $this->update->getMessage();
 
-        $this->account = $this->message->from;
+        $this->account = $this->message->isNotEmpty() && $this->message->has('from') ? $this->message->from : null; // prevent error in inline queries
 
-        $this->chatId = $this->message->getChat()->getId();
+        $this->chatId = $this->message->isNotEmpty() ? $this->message?->getChat()?->getId() : null; // prevent error in inline queries
     }
 
-    public function handle(Request $request, TelegramBotService $telegramBotService, SongService $songService, AiInterface $aiService)
+    public function handle(TelegramBotService $telegramBotService, SongService $songService, AiInterface $aiService)
     {
         try {
             // get user by telegram username 
             $user = $this->getUser($this->account->username);
 
+            // check for callback queries
             if ($this->update->has('callback_query')) {
                 $callbackQuery = $this->update->getCallbackQuery();
                 $callbackData = $callbackQuery->getData();
@@ -49,9 +47,36 @@ class TelegramBotController extends Controller
                 return;
             }
 
+            if ($this->update->has('inline_query')) {
+                $inlineQuery = $this->update->getInlineQuery();
+                $queryText = $inlineQuery->get('query');
+
+                if (empty($queryText))
+                    return;
+
+                $telegramBotService->searchSongsInlineQuery($inlineQuery, $queryText, $this->chatId);
+
+                return;
+
+            }
+
+            $payload = str_starts_with($this->message->getText(), '/start ')
+                ? str_replace('/start ', '', $this->message->getText())
+                : null;
+
+            if ($payload) {
+                $this->handlePayload($this->chatId, $payload);
+                return;
+            }
+
+            // send start message and return
             if ($this->message->getText() === '/start') {
-                $this->sendWelcomeMessage($user?->id);
-            } elseif ($this->message->getAudio()) {
+                $telegramBotService->sendWelcomeMessage($this->telegram, $this->chatId, $user?->id, $this->account->username);
+                return;
+            }
+
+            // get audio and upload to site
+            if ($this->message->getAudio()) {
                 // user isn't registered
                 if (!$user) {
                     $this->telegram->sendMessage([
@@ -61,214 +86,37 @@ class TelegramBotController extends Controller
                     return;
                 }
 
-                // get audio
-                $audio = $this->message->getAudio();
-
-                // get file
-                $fileId = $audio->getFileId();
-
-                // send loading text
-                $songUploadingMessage = $this->telegram->sendMessage([
-                    'chat_id' => $this->chatId,
-                    'text' => "Uploading {$audio->getTitle()}...",
-                ]);
-
-                // get file url
-                $file = $this->telegram->getFile(['file_id' => $fileId]);
-
-                $fileUrl = $telegramBotService::getFileUrl($file);
-
-                //  check for upload limitation
-                if (!$user->canUpload($audio->getFileSize())) {
-                    $this->telegram->sendMessage([
-                        'chat_id' => $this->chatId,
-                        'text' => "🔥 You have reached your upload limit 3GB  \n Please send message to t.me/p_nightwolf",
-                    ]);
-                    return;
-                }
-
-                $song = $songService->createSongFromTelegramBot($fileUrl, $audio, $user);
-
-                // response success message
-                $songUploadedMessage = $this->telegram->sendMessage([
-                    'chat_id' => $this->chatId,
-                    'text' => "🟢 Song has been uploaded successfully. wait for link....",
-                ]);
-
-                //  delete and cleanup messages
-                $this->telegram->deleteMessage([
-                    'chat_id' => $this->chatId,
-                    'message_id' => $songUploadingMessage->getMessageId(),
-                ]);
-                $this->telegram->deleteMessage([
-                    'chat_id' => $this->chatId,
-                    'message_id' => $songUploadedMessage->getMessageId(),
-                ]);
-
-
-                $this->telegram->sendMessage([
-                    'chat_id' => $this->chatId,
-                    'text' => "🎧 Song:\n {$song->name} \n {$song->direct_link}",
-                    'reply_markup' => Keyboard::make([
-                        'inline_keyboard' => [
-                            [
-                                ['text' => 'Add To Playlist', 'callback_data' => "showPlaylists:{$user->telegram_username}:{$song->id}"],
-                            ]
-                        ]
-                    ])
-                ]);
-
-                return;
-            } else {
-                if ($user && is_string($this->message->getText())) {
-                    $text = $this->aiResponseBaseOnUserData($aiService, $this->message->getText());
-                    $this->telegram->sendMessage([
-                        'chat_id' => $this->chatId,
-                        'text' => $text,
-                    ]);
-                    return;
-                }
-
-                $this->commandNotFound();
+                // upload song and send success message
+                $telegramBotService->uploadSongFromBotToSite($this->telegram, $this->chatId, $this->message, $user, $telegramBotService, $songService);
                 return;
             }
+
+            // user send text message 
+            if ($user && is_string($this->message->getText())) {
+                $text = $telegramBotService->aiResponseBaseOnUserData($aiService, $this->message->getText());
+                $this->telegram->sendMessage([
+                    'chat_id' => $this->chatId,
+                    'text' => $text,
+                ]);
+                return;
+            }
+
+            $telegramBotService->commandNotFound($this->telegram, $this->chatId);
+            return;
+
         } catch (\Throwable $th) {
             $this->telegram->sendMessage([
                 'chat_id' => $this->chatId,
-                'text' => $th->getMessage() . ' in line:' . $th->getLine(),
+                //'text' => "Some thing went wrong. If you think this is a bug contact support please.",
+                'text' => $th->getMessage() . " - " . $th->getLine(),
             ]);
             return;
         }
     }
 
-    /**
-     * send welcome message 
-     * @param int|null $userId
-     * @return void
-     */
-    public function sendWelcomeMessage(int|null $userId)
-    {
-        $welcomeText = "👋 Hello {$this->account->username}. \n\n";
-
-        if ($userId) {
-            $welcomeText .= "🔼 Please send me a song file to upload it. \n\n";
-            $welcomeText .= "👉 Maximum size due telegram limitation is 20MB.";
-        } else {
-            $welcomeText .= "😥 Your account isn't registered t.me/p_nightwolf";
-        }
-
-
-        $this->telegram->sendMessage([
-            'chat_id' => $this->chatId,
-            'text' => $welcomeText,
-        ]);
-    }
-
-
-    public function commandNotFound()
-    {
-        $this->telegram->sendMessage([
-            'chat_id' => $this->chatId,
-            'text' => "😐 hmmm what are you talking about? Send me a song",
-        ]);
-    }
-
-    public function aiResponseBaseOnUserData(AiInterface $aiService, string $userAskedRequest)
-    {
-        $prompt = config('app.ai_search_prompt');
-        $prompt .= $userAskedRequest;
-        $aiResponse = $aiService->generateContent($prompt);
-        $arrayResponse = $aiService->textJsonToArray($aiResponse);
-
-        if (is_array($arrayResponse) && !empty($arrayResponse['type']) && !empty($arrayResponse['value'])) {
-            $type = $arrayResponse['type'];
-            $value = $arrayResponse['value'];
-
-            if ($type === 'link') {
-                $songs = Song::where('name', 'like', "%{$value}%")->orWhere('artist', 'like', "%{$value}%")->take(20)->get(['id', 'name', 'lyrics', 'artist']);
-                $message = $this->responseMessage($songs, 'link');
-                return $message;
-            }
-
-            if ($type === 'lyrics') {
-                $songs = Song::where('name', 'like', "%{$value}%")->take(1)->get(['id', 'name', 'lyrics', 'artist']);
-                $message = $this->responseMessage($songs, 'lyrics');
-                return $message;
-            }
-
-            if ($type === 'playlist') {
-                $playlists = Playlist::where('name', 'like', "%{$value}%")->take(20)->get(['id', 'name']);
-                $message = $this->responseMessage($playlists, 'playlist');
-                return $message;
-            }
-
-
-            throw new \Exception('Invalid type', 400);
-        }
-
-        return "Sorry I can't understand your request 😥";
-    }
-
-    protected function responseMessage(Collection $data, string $type = '')
-    {
-        if (!$data->count())
-            return $message = "Sorry I didn't find any thing 🔎";
-
-        $message = '';
-
-        if ($type === 'playlist') {
-            if ($data->count() > 1)
-                $message = "🟣 Here is founded playlists: \n\n";
-            foreach ($data as $key => $playlist) {
-                $key++;
-                $message .= "$key. 🎶 {$playlist->name} \n 🔗 {$playlist->directLink} \n\n";
-            }
-        }
-
-        if ($type === 'link') {
-            if ($data->count() > 1)
-                $message = "🟣 Here is founded songs: \n\n";
-
-            foreach ($data as $key => $song) {
-                $key++;
-                $message .= "$key. 🎧 {$song->name} \n 🔗 {$song->directLink} \n\n";
-            }
-        }
-
-        if ($type === 'lyrics') {
-            foreach ($data as $song) {
-                $message .= "🎧 {$song->name} \n 🔗 {$song->directLink} \n\n {$song->lyrics}";
-            }
-        }
-
-        return $message;
-    }
-
     public function getUser($telegramUsername)
     {
         return User::firstWhere('telegram_username', $telegramUsername);
-    }
-
-    public function playlistsInlineKeyboard($user, $buttonsPerRow = 2, $additionalData = null)
-    {
-        $playlists = $user->playlists;
-        $buttons = [];
-
-
-        foreach ($playlists as $playlist) {
-            $callbackData = "addToPlaylist:{$user->telegram_username}:{$playlist->id}";
-
-            if ($additionalData)
-                $callbackData .= (string) $additionalData;
-
-            $buttons[] = [
-                'text' => $playlist->name,
-                'callback_data' => $callbackData,
-            ];
-        }
-
-        // buttons per row 
-        return array_chunk($buttons, $buttonsPerRow);
     }
 
     public function runCommand($command)
@@ -286,37 +134,42 @@ class TelegramBotController extends Controller
     public function showPlaylists($telegramUsername, $songId)
     {
         $user = $this->getUser($telegramUsername);
-        $playlistsInlineKeyboard = $this->playlistsInlineKeyboard($user, 4, ":$songId");
-
-        $replyMarkup = Keyboard::make([
-            'inline_keyboard' => $playlistsInlineKeyboard
-        ]);
-        $this->telegram->sendMessage([
-            'chat_id' => $this->chatId,
-            'text' => "Select your playlist 👇",
-            'reply_markup' => $replyMarkup,
-        ]);
+        (new TelegramBotService())->showPlaylistsHandler($this->telegram, $this->chatId, $user, $songId);
     }
 
     public function addToPlaylist($telegramUsername, $playlistId, $songId)
     {
         $user = $this->getUser($telegramUsername);
-        $playlist = Playlist::findOrFail($playlistId);
-        $song = Song::findOrFail($songId);
+        (new TelegramBotService())->addToPlaylistHandler($this->telegram, $this->chatId, $user, $playlistId, $songId);
+    }
 
-        if ($song->user_id !== $user->id || $playlist->user_id !== $user->id) {
-            $this->telegram->sendMessage([
-                'chat_id' => $this->chatId,
-                'text' => "Only owner can make these changes",
-            ]);
-            return;
-        }
-
-        $playlist->songs()->attach($song);
+    public function sendSongToTelegram($songId)
+    {
         $this->telegram->sendMessage([
             'chat_id' => $this->chatId,
-            'text' => "✅ Song added to playlist successfully",
+            'text' => "Song id is: $songId",
         ]);
-        return;
+    }
+
+    public function handlePayload($chatId, string $payload)
+    {
+        [$command, $songId] = explode('_', $payload);
+
+        $song = Song::firstWhere('id', $songId);
+
+        $songUrl = config('app.app_url') . "/storage/{$song->path}";
+
+        $params = [
+            'chat_id' => $chatId,
+            'audio' => InputFile::create($songUrl),
+            'caption' => "[🟣 Myplaylists](https://t.me/myplaylists_ir)",
+            'parse_mode' => 'Markdown'
+        ];
+
+        $params['title'] ??= $song->name;
+        $params['performer'] ??= $song->artist;
+        $params['thumb'] ??= InputFile::create($song->cover);
+
+        $this->telegram->sendAudio($params);
     }
 }
--- app/Services/TelegramBotService.php
+++ app/Services/TelegramBotService.php
@@ -1,8 +1,15 @@
 <?php
 namespace App\Services;
 
+use App\Models\Playlist;
+use App\Models\Song;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Telegram\Bot\Objects\File;
+use App\Interfaces\AiInterface;
+use Illuminate\Database\Eloquent\Collection;
+use Telegram\Bot\Keyboard\Keyboard;
+use Telegram\Bot\Objects\InlineQuery\InlineQueryResultArticle;
+use Telegram\Bot\Laravel\Facades\Telegram;
 
 class TelegramBotService
 {
@@ -17,5 +24,263 @@ class TelegramBotService
                 'error' => $e->getMessage(),
             ], 500);
         }
+    }
+
+    /**
+     * send welcome message 
+     * @param int|null $userId
+     * @return void
+     */
+    public function sendWelcomeMessage($telegram, $chatId, int|null $userId, string $telegramUsername)
+    {
+        $welcomeText = "👋 Hello {$telegramUsername}. \n\n";
+
+        if ($userId) {
+            $welcomeText .= "🔼 Please send me a song file to upload it. \n\n";
+            $welcomeText .= "👉 Maximum size due telegram limitation is 20MB.";
+        } else {
+            $welcomeText .= "😥 Your account isn't registered t.me/p_nightwolf";
+        }
+
+
+        $telegram->sendMessage([
+            'chat_id' => $chatId,
+            'text' => $welcomeText,
+        ]);
+    }
+
+    public function commandNotFound($telegram, $chatId)
+    {
+        $telegram->sendMessage([
+            'chat_id' => $chatId,
+            'text' => "😐 hmmm what are you talking about? Send me a song",
+        ]);
+    }
+
+    public function aiResponseBaseOnUserData(AiInterface $aiService, string $userAskedRequest)
+    {
+        $prompt = config('app.ai_search_prompt');
+        $prompt .= $userAskedRequest;
+        $aiResponse = $aiService->generateContent($prompt);
+        $arrayResponse = $aiService->textJsonToArray($aiResponse);
+
+        if (is_array($arrayResponse) && !empty($arrayResponse['type']) && !empty($arrayResponse['value'])) {
+            $type = $arrayResponse['type'];
+            $value = $arrayResponse['value'];
+
+            if ($type === 'link') {
+                $songs = Song::where('name', 'like', "%{$value}%")->orWhere('artist', 'like', "%{$value}%")->take(20)->get(['id', 'name', 'lyrics', 'artist']);
+                $message = $this->responseMessage($songs, 'link');
+                return $message;
+            }
+
+            if ($type === 'lyrics') {
+                $songs = Song::where('name', 'like', "%{$value}%")->take(1)->get(['id', 'name', 'lyrics', 'artist']);
+                $message = $this->responseMessage($songs, 'lyrics');
+                return $message;
+            }
+
+            if ($type === 'playlist') {
+                $playlists = Playlist::where('name', 'like', "%{$value}%")->take(20)->get(['id', 'name']);
+                $message = $this->responseMessage($playlists, 'playlist');
+                return $message;
+            }
+
+
+            throw new \Exception('Invalid type', 400);
+        }
+
+        return "Sorry I can't understand your request 😥";
+    }
+
+    public function responseMessage(Collection $data, string $type = '')
+    {
+        if (!$data->count())
+            return $message = "Sorry I didn't find any thing 🔎";
+
+        $message = '';
+
+        if ($type === 'playlist') {
+            if ($data->count() > 1)
+                $message = "🟣 Here is founded playlists: \n\n";
+            foreach ($data as $key => $playlist) {
+                $key++;
+                $message .= "$key. 🎶 {$playlist->name} \n 🔗 {$playlist->directLink} \n\n";
+            }
+        }
+
+        if ($type === 'link') {
+            if ($data->count() > 1)
+                $message = "🟣 Here is founded songs: \n\n";
+
+            foreach ($data as $key => $song) {
+                $key++;
+                $message .= "$key. 🎧 {$song->name} \n 🔗 {$song->directLink} \n\n";
+            }
+        }
+
+        if ($type === 'lyrics') {
+            foreach ($data as $song) {
+                $message .= "🎧 {$song->name} \n 🔗 {$song->directLink} \n\n {$song->lyrics}";
+            }
+        }
+
+        return $message;
+    }
+
+    public function uploadSongFromBotToSite($telegram, $chatId, $message, $user, $telegramBotService, $songService)
+    {
+        // get audio
+        $audio = $message->getAudio();
+
+        // get file
+        $fileId = $audio->getFileId();
+
+        // send loading text
+        $songUploadingMessage = $telegram->sendMessage([
+            'chat_id' => $chatId,
+            'text' => "Uploading {$audio->getTitle()}...",
+        ]);
+
+        // get file url
+        $file = $telegram->getFile(['file_id' => $fileId]);
+
+        $fileUrl = $telegramBotService::getFileUrl($file);
+
+        //  check for upload limitation
+        if (!$user->canUpload($audio->getFileSize())) {
+            $telegram->sendMessage([
+                'chat_id' => $chatId,
+                'text' => "🔥 You have reached your upload limit 3GB  \n Please send message to t.me/p_nightwolf",
+            ]);
+            return;
+        }
+
+        $song = $songService->createSongFromTelegramBot($fileUrl, $audio, $user);
+
+        // response success message
+        $songUploadedMessage = $telegram->sendMessage([
+            'chat_id' => $chatId,
+            'text' => "🟢 Song has been uploaded successfully. wait for link....",
+        ]);
+
+        //  delete and cleanup messages
+        $telegram->deleteMessage([
+            'chat_id' => $chatId,
+            'message_id' => $songUploadingMessage->getMessageId(),
+        ]);
+        $telegram->deleteMessage([
+            'chat_id' => $chatId,
+            'message_id' => $songUploadedMessage->getMessageId(),
+        ]);
+
+
+        $telegram->sendMessage([
+            'chat_id' => $chatId,
+            'text' => "🎧 Song:\n {$song->name} \n {$song->direct_link}",
+            'reply_markup' => Keyboard::make([
+                'inline_keyboard' => [
+                    [
+                        ['text' => 'Add To Playlist', 'callback_data' => "showPlaylists:{$user->telegram_username}:{$song->id}"],
+                    ]
+                ]
+            ])
+        ]);
+    }
+
+    public function playlistsInlineKeyboard($user, $buttonsPerRow = 2, $additionalData = null)
+    {
+        $playlists = $user->playlists;
+        $buttons = [];
+
+
+        foreach ($playlists as $playlist) {
+            $callbackData = "addToPlaylist:{$user->telegram_username}:{$playlist->id}";
+
+            if ($additionalData)
+                $callbackData .= ":$additionalData";
+
+            $buttons[] = [
+                'text' => $playlist->name,
+                'callback_data' => $callbackData,
+            ];
+        }
+
+        // buttons per row 
+        return array_chunk($buttons, $buttonsPerRow);
+    }
+
+    public function showPlaylistsHandler($telegram, $chatId, $user, $songId)
+    {
+        $playlistsInlineKeyboard = $this->playlistsInlineKeyboard($user, 4, $songId);
+
+        $replyMarkup = Keyboard::make([
+            'inline_keyboard' => $playlistsInlineKeyboard
+        ]);
+        $telegram->sendMessage([
+            'chat_id' => $chatId,
+            'text' => "Select your playlist 👇",
+            'reply_markup' => $replyMarkup,
+        ]);
+    }
+
+    public function addToPlaylistHandler($telegram, $chatId, $user, $playlistId, $songId)
+    {
+        $playlist = Playlist::findOrFail($playlistId);
+        $song = Song::findOrFail($songId);
+
+        if ($song->user_id !== $user->id || $playlist->user_id !== $user->id) {
+            $telegram->sendMessage([
+                'chat_id' => $chatId,
+                'text' => "Only owner can make these changes",
+            ]);
+            return;
+        }
+
+        $playlist->songs()->attach($song);
+        $telegram->sendMessage([
+            'chat_id' => $chatId,
+            'text' => "✅ Song added to playlist successfully",
+        ]);
+    }
+
+    public function searchSongsInlineQuery($inlineQuery, $queryText, $chatId)
+    {
+        $songs = Song::where('name', 'LIKE', "%$queryText%")
+            ->orWhere('artist', 'LIKE', "%$queryText%")
+            ->limit(10)
+            ->get();
+
+        $results = [];
+
+        foreach ($songs as $song) {
+            $album = $song->album ?? 'unknwon';
+            $artist = $song->artist ?? 'unknown';
+
+            $results[] = new InlineQueryResultArticle([
+                'id' => (string) $song->id,
+                'title' => $song->name . " - " . $chatId,
+                'description' => $song->artist ?? $song->album,
+                'input_message_content' => [
+                    'message_text' => "🎧 *{$song->name}* \n🗣{$artist}  \n💽 {$album} \n🟣 [Open Application]({$song->direct_link})",
+                    'parse_mode' => 'Markdown',
+                ],
+                'reply_markup'=>Keyboard::make([
+                    'inline_keyboard' => [
+                        [
+                            ['text' => '🎧 Listen', 'url' => $song->direct_link],
+                            ['text' => '📥 Download', 'url' => "https://t.me/Myplaylists_ir_Bot?start=sendSongToTelegram_{$song->id}"]
+                        ]
+                    ]
+                ])
+
+            ]);
+        }
+
+        Telegram::answerInlineQuery([
+            'inline_query_id' => $inlineQuery->get('id'),
+            'results' => json_encode($results),
+            'cache_time' => 0,
+        ]);
     }
 }
