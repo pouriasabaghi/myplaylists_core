53f3aefe79138747e4da89be25410934a2b3eb75
Sajjad Dashti Nezhad <saggad.dashti@gmail.com>
2025-03-23 17:12:01 +0400

    Adding Esperanto language
    Adding Esperanto language support for the bot's guide tour.

---
+++ lang/eo/tour.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    "steps" => [
+        "📥 Rekte elŝutu de SoundCloud kaj Youtube Music \n\n🔹Vi simple devas sendi la kundivideblan ligilon de la kanto de SoundCloud aŭ YouTube Music (YouTube) al la roboto, kaj ĝi elŝutos kaj sendos la kanton al vi! \n\nNi vidu ekzemplon ➡️",
+        "[Rigardu ĉi tie 👇](https://t.me/myplaylists_ir/74)", // SoundCloud and Youtube Music
+        "🔎 Serĉu kaj elŝutu el retejo-arkivo \n\n🔹Vi povas serĉi retejo-arkivon kaj elŝuti ajnan kanton, kiun vi volas per la roboto! \n\n🙂 Nur sendu kantonomon aŭ artiston kiel: gary moore\n\nNi vidu ekzemplon ➡️",
+        "[Rigardu ĉi tie 👇](https://t.me/myplaylists_ir/75)", // Site Archive Download
+        "🤔 Serĉu rete \n\n🔹Se la kanto, kiun vi volas, ne ekzistas en la arkivo, neniu problemo, ĉiam vi povas serĉi rete. \n\nNi vidu ekzemplon ➡️",
+        "[Rigardu ĉi tie 👇](https://t.me/myplaylists_ir/76)", // Internet Search
+        "☄️ Kunhavigu ĉie \n\n🔹Vi povas serĉi kanton en iu ajn babilejo en kiu vi estas (privata babilejo aŭ grupo) kaj kunhavigi la kanton, kiun vi volas, kun aliaj.\n🔹Aliaj uzantoj povas aŭskulti la kunhavigitan kanton aŭ elŝutante ĝin per la roboto aŭ vizitante la retejon.\n\n🤔 Retejo? ni fokusos ĝin poste! \n\nNi vidu ekzemplon ➡️",
+        "[Rigardu ĉi tie 👇](https://t.me/myplaylists_ir/77)", // Add To Playlists 
+        "🟣 MyPlaylists\n\n🔹MyPlaylists estas muzika platformo, kie vi povas krei kaj sekvi ludlistojn, alŝuti kantojn, aldoni al viaj plej ŝatataj, esplori novan muzikon kaj pli. \n\nNi vidu ekzemplon ➡️",
+        "[Rigardu ĉi tie 👇](https://t.me/myplaylists_ir/79)", // Myplaylists demo
+        "🙂 Dankon pro esti kun mi en ĉi tiu turneo.\n\n💫 Klaku ĉi tie kaj aŭskultu vian unuan kanton\n/dl_792\n\n🟣 Abonu nian kanalon por pliaj ĝisdatigoj \nt.me/myplaylists_ir\n\n📱 Rigardu nian retaplikaĵon\nhttps://myplaylists.ir",
+    ],
+    "start_button" => "Eku",
+    "try_out_button"=> "☄️ Provu",
+    "introduction" => "👋 Hej! Mi estas MyPlaylists-Bot, Via muzika roboto kun ĉiaj bonegaj funkcioj.\n\n💫 Ni ekiru rapidan turneon, kaj mi montros al vi kiel uzi min!",
+    "rules_and_limitations"=>"👉 Maksimuma grandeco pro telegrama limigo estas 20MB.",
+    "end_button"=>"Fino de la turneo",
+];
