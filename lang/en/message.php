824fe3b1b09b1a2a15d5921f940258b42fd541c5
pouriasabaghi <pouriasabaghi@gmail.com>
2025-03-23 23:23:10 +0330

    chore: translate

--- lang/en/message.php
+++ lang/en/message.php
@@ -3,7 +3,7 @@
 return [
     "need_access" => "To upload a song via the bot, you need an access token. You will be redirected to the application; if necessary, log in and obtain your access token, then send it to the bot.",
     "token_button" => "Get Bot Access 🔑",
-    "telegram_login"=>"📲 To continue and log into your account, please click the button below. \n💡By clicking the button, you will be connected to your account. \n\n👇 Tap here to log in:",
+    "telegram_login"=>"⚠️ Before continuing please turn of In-App browser in Chat Settings tg://settings \n\n📲 To continue and log into your account, please click the button below. \n\n💡By clicking the button, you will be connected to your account. \n\n👇 Tap here to log in:",
     "login_button"=>"Login",
     "access_alert"=> "🔑 Your access key has been copied to your clipboard.Please send it to me.\n⚠️ This token expire after 60 second, If your toked expired generate new one.\n\n",
     "access_granted"=>"🔑 You access updated \n\n🟣 Now you have access to upload your songs to MyPlaylists. \n\n🔼 Please send me a song file to upload it. \n\n👉 Maximum size due telegram limitation is 20MB.",
