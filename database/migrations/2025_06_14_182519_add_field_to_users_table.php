9ad60edd36c33031e9933cab15c4889796a4f105
pouriasabaghi <pouriasabaghi@gmail.com>
2025-06-14 21:56:15 +0330

    chore: change migration name

--- database/migrations/2025_06_14_182519_add_field_to_users_table.php
+++ database/migrations/2025_06_14_182519_add_field_to_users_table.php
@@ -4,14 +4,15 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::table('user', function (Blueprint $table) {
-            $table->string('bio');
+        Schema::table('users', function (Blueprint $table) {
+            $table->string(column: 'bio')->nullable();
         });
     }
 
@@ -20,7 +21,7 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::table('user', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->dropColumn('bio');
         });
     }
